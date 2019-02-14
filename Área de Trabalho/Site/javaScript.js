23be8a8d5bb1bba91ea2511adfcbc00779bf9873
joao <joao12308@hotmail.com>
2019-02-14 20:35:36 -0200

    Relembrando Css,Html e Js

---
+++ Área de Trabalho/Site/javaScript.js
@@ -0,0 +1,7 @@
+function calcular() {
+  var n1 = parseInt(document.getElementById('numero1').value, 10);
+  var n2 = parseInt(document.getElementById('numero2').value, 10);
+  document.getElementById('resultado').innerHTML = n1 + n2;
+
+alert(n1 + n2);
+}
